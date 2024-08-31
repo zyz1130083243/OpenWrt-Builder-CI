43a79eabacc4db2b56aee4871167482881cc247a
zyz <31105177+zyz1130083243@users.noreply.github.com>
2024-08-31 15:47:58 +0800

    Update feeds_config.sh

--- feeds_config.sh
+++ feeds_config.sh
@@ -2,4 +2,4 @@
 
 # Add feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/feeds/luci/themes/argon-theme
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/feeds/luci/themes/luci-theme-argon
